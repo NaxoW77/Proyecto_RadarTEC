ec6ee7089710d466572701c5ba15d03b86ab82ce
JDNY-GH <jordannysf@gmail.com>
2026-06-06 17:58:13 -0600

    Método de cálculo de parábola y diseño de GUI

--- Radar_GUI/__main__.py
+++ Radar_GUI/__main__.py
@@ -5,41 +5,68 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import tkinter as tk
 from threading import Thread
 import numpy as np
+from math import sin, cos
 
 SERIAL_PORT = 'COM7' 
 BAUD_RATE = 9600
 
 class Radar:
     def __init__(self, root):
+
+        #Crear gráfico circular del radar
         self.root = root
         self.root.title("Radar")
         
-        self.fig, self.ax = plt.subplots(subplot_kw={'projection': 'polar'})
-        self.ax.set_ylim(0, 50)
+        self.figRad, self.axRad = plt.subplots(subplot_kw={'projection': 'polar'})
+        self.axRad.set_title('Detección del radar', color='green')
+        self.axRad.grid(color='green')
+        self.axRad.set_facecolor('black')
+        self.axRad.set_ylim(0, 50)
         
-        self.ax.set_xlim(0, np.pi)
-        self.ax.set_xticks(np.linspace(0, np.pi, 7))
-        self.ax.set_xticklabels(['0°', '30°', '60°', '90°', '120°', '150°', '180°'])
+        self.axRad.set_xlim(0, np.pi)
+        self.axRad.set_xticks(np.linspace(0, np.pi, 7))
+        self.axRad.set_xticklabels(['0°', '30°', '60°', '90°', '120°', '150°', '180°'], color='green')
         
-        self.ax.set_yticklabels([]) 
-        self.ax.set_yticks([10, 20, 30, 40, 50])
+        self.axRad.set_yticklabels([]) 
+        self.axRad.set_yticks([10, 20, 30, 40, 50])
         
-        offset = 0.0 
+        offset = 0.0
         
         for r in [10, 20, 30, 40, 50]:
-            self.ax.text(-offset, r, str(r), ha='center', va='top', color='black', fontsize=9)
+            self.axRad.text(-offset, r, str(r), ha='center', va='top', color='green', fontsize=9)
             
         for r in [10, 20, 30, 40, 50]:
-            self.ax.text(np.pi + offset, r, str(r), ha='center', va='top', color='black', fontsize=9)
+            self.axRad.text(np.pi + offset, r, str(r), ha='center', va='top', color='green', fontsize=9)
             
-        self.ax.text(-np.pi/2, 2, '0', ha='center', va='top', color='black', fontsize=9)
+        self.axRad.text(-np.pi/2, 2, '0', ha='center', va='top', color='green', fontsize=9)
         
-        self.line, = self.ax.plot([], [], 'ro', markersize=5)
+        self.line, = self.axRad.plot([], [], 'ro', markersize=5)
         self.points_x = []
         self.points_y = []
 
-        self.canvas = FigureCanvasTkAgg(self.fig, master=root)
-        self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+        self.canvasRad = FigureCanvasTkAgg(self.figRad, master=root)
+        self.canvasRad.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
+        self.figRad.set_facecolor("black")
+
+        #Crear gráfico del plano cartesiano
+
+        self.figCart, self.axCart = plt.subplots()
+        self.axCart.plot(color="green")
+        self.axCart.grid(color="green")
+        self.axCart.set_facecolor('black')
+        self.axCart.set_xlabel('Tiempo (x)', color='green')
+        self.axCart.set_ylabel('Profundidad (y)', color='green')
+        self.axCart.tick_params('both', colors='green')
+        self.axCart.set_title('Predicción Parabólica', color='green')
+        self.axCart.spines['top'].set_color('green')
+        self.axCart.spines['bottom'].set_color('green')
+        self.axCart.spines['right'].set_color('green')
+        self.axCart.spines['left'].set_color('green')
+
+
+        self.canvasCart = FigureCanvasTkAgg(self.figCart, master=root)
+        self.canvasCart.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
+        self.figCart.set_facecolor("black")
 
         self.running = True
         self.thread = Thread(target=self.read_serial, daemon=True)
@@ -77,9 +104,39 @@ class Radar:
     def update_plot(self):
         try:
             self.line.set_data(self.points_x, self.points_y)
-            self.canvas.draw_idle()
+            self.canvasRad.draw_idle()
+
+
         except:
             pass
+    
+    def parable(self, pol1, pol2, time, const=9.8):
+
+        """Grafica una parábola según dos coordenadas polares dadas,
+        el tiempo de deteccion entre una y otra, y una constante
+            - pol1: Coordenada polar #1. Tupla en forma (x, y). siendo 'x' la distancia y 'y' el ángulo
+            - pol2: Coordenada polar #2. Mismas condiciones que pol1.
+            - time: Tiempo de detección entre una coordenada y otra
+            - const: Constante para la parábola, por defecto es la constante gravitatoria: 9.8"""
+
+        #Transformar las coordenadas polares en cartesianas, y asignar cada valor 'x' y 'y'
+        #por separado
+
+        x1 = pol1[0]*cos(pol1[1])
+        x2 = pol2[0]*cos(pol2[1])
+
+        y1 = pol1[0]*sin(pol1[1])
+        y2 = pol2[0]*sin(pol2[1])
+        
+        #Calcular coeficientes de la variable independiente
+
+        a = - (const * (time**2)) / (2 * ((x2 - x1)**2))
+        b = (y2 - y1 + 0.5 * const * (time**2)) / (x2 - x1) - 2*a*x1
+        c = y1 - a * (x1**2) - b * x1
+
+
+
+        
 
     def on_close(self):
         self.running = False
