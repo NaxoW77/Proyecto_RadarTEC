672cb299cae33db10e7c24aac522ff527148044b
Ignacio Apuy <ignacioapuy@gmail.com>
2026-06-10 13:07:22 -0600

    Comentarios varios

--- Radar_Pos0/Radar_Pos0.ino
+++ Radar_Pos0/Radar_Pos0.ino
@@ -1,14 +1,29 @@
+//
+//   Código simple para ubicar el servo
+//
+//   Se utiliza para colocar el servo en
+//   una posición específica durante
+//   el ensamblado de las piezas.
+//
+
+// Imports necesarios
 #include <Servo.h>
 
+// Definiciones de pines
 const int servoPin = 7;
 
+// Inicialización del servo
 Servo myServo;
 
+// Función setup
 void setup() {
   Serial.begin(9600);
   myServo.attach(servoPin);
 }
 
+// Función loop
 void loop() {
+
+  // Ubicación del servo
   myServo.write(0);
 }
